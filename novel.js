5e1fd6466bf514a02b4b94ab9fbfa99d30d53939
xinyangmaojian666 <1598286002@qq.com>
2024-09-10 21:49:17 +0800

    666

--- novel.js
+++ novel.js
@@ -5,13 +5,16 @@ const fetch = url => {
   axios
     .get(url)
     .then(res => {
+      // 使用 Cheerio 加载 HTML
       const $ = cheerio.load(res.data)
-      console.log($('#list a').html())
 
-      //   const links = $('#list a')
-      //   for (let i = 0; i < links.length; i++) {
-      //     console.log(links[i])
-      //   }
+      // 找到 ID 为 'list' 的 div 下的所有 a 标签
+      const links = $('#list a')
+
+      // 遍历并打印每个 a 标签的 href 属性
+      links.each((index, element) => {
+        console.log($(element).attr('href'))
+      })
     })
     .catch(error => {
       console.error('Error fetching the URL:', error)
